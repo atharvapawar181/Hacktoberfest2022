18e23e926e08df06e1ef2b1d653f53ba82bc166d
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2022-10-04 23:54:26 +0530

    Update bubbleSort.cpp

--- C pp/sort/bubbleSort.cpp
+++ C pp/sort/bubbleSort.cpp
@@ -6,13 +6,7 @@ void bubbleSort(vector<int>& arr, int n)
         //for round 1 to n-1
         bool swapped = false;
         
-        for(int j =0; j<n-i; j++) {
-            
-            //process element till n-i th index
-            if(arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
-                swapped = true;
-            }
+       
             
         }
         
