0e62a7d35c876ec0bdff8eb5549b9a9d561db0da
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2022-10-04 23:25:50 +0530

    Create swap_Alternate.cpp

---
+++ C pp/Array/swap_Alternate.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+void printArray(int arr[], int n) {
+
+    for(int i = 0; i<n; i++ ) {
+        cout<< arr[i] <<" ";
+    }
+    cout<<endl;
+}
+
+void swapAlternate(int arr[], int size) {
+
+    for(int i = 0; i<size; i+=2 ) {    //i+=2 is used as, aletrnate element is to be swaped
+        if(i+1 < size) {
+            swap(arr[i], arr[i+1]);
+        }
+    }
+
+}
+
+
+int main(){
+
+  int even[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 99};
+  int odd[5]= {1 ,2, 3, 4, 5};
+
+  swapAlternate(even, 10);
+  printArray(even, 10);
+
+
+  swapAlternate(odd, 5);
+  printArray(odd, 5);
+
+  return 0;
+}
