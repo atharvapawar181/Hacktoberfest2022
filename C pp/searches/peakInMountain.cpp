78411fcb5efcb85d0c8965e8dd91619692940522
adnansayyed91 <91146582+adnansayyed91@users.noreply.github.com>
2022-10-05 12:04:44 +0530

    Update peakInMountain.cpp

--- C pp/searches/peakInMountain.cpp
+++ C pp/searches/peakInMountain.cpp
@@ -8,6 +8,16 @@ public:
 	int s = 0, e = v.size() - 1;
 	int mid=(s + e) / 2;
 	
+while (s < e)
+	{
+		if(v[mid] < v[mid+1])
+     			s=mid+1;
+    		else
+      			e = mid;
+    
+		mid = (s + e) / 2;
+	}
+	return s;
     }
     int peakIndexInMountainArray(vector<int>& arr) {
         return find_pivot(arr);
